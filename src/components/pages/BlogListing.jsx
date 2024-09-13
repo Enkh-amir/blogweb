765a0fcb4b9ca925f8100aeec321c346c7c2a1e1
Энх-Амар Болдбаатар <LPC240855@G401-CC0090.local>
2024-09-13 12:09:49 +0800

    small changes

--- src/components/pages/BlogListing.jsx
+++ src/components/pages/BlogListing.jsx
@@ -36,10 +36,10 @@ export const BlogListing = () => {
             </div>
             <div className="flex flex-col p-2 w-[344px] h-full justify-between">
               <div className="px-[10px] py-1 bg-custom-blue w-max font-wsans text-sm font-medium text-[#4B6BFB]">
-                {item.tag_list[0]}
+                {item.tag_list[0] || "no tag"}
               </div>
               <div className="text-[#181A2A] text-2xl leading-7 font-medium font-wsans line-clamp-3">
-                {item.title}
+                {item.title || "no title"}
               </div>
               <div className="font-wsans flex gap-5 items-center font-medium text-base text-[#97989F]">
                 <div className="flex gap-3 text-[#97989F] items-center font-wsans text-base font-medium">
